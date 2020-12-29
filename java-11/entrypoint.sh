19c55b8b199e557cb22dbb1db3706a5032416fb6
alexeylesin <alexxxlyt@yandex.ru>
2020-12-29 13:30:14 +0300

    Removed path echo

--- java-11/entrypoint.sh
+++ java-11/entrypoint.sh
@@ -1,8 +1,8 @@
 cd /home/container
 
-echo "Starting container, script by alexeylesin..."
+echo "Starting container, script by vk.com/securityerror for MineHosting.ru"
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+# echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 ${MODIFIED_STARTUP}
